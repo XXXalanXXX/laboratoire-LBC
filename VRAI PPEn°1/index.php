82db986e5ad977072dea57ce8b137ec9d784633f
SIO-SM-49\gourdona <gourdona49@gmail.com>
2017-10-05 10:54:01 +0200

    vrai index + vue du header

--- VRAI PPEn°1/index.php
+++ VRAI PPEn°1/index.php
@@ -1,10 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="style/style.css"/>
-</head>
-<body>
+<?php
+session_start();
+require_once("util/fonctions.inc.php");
+require_once("util/class.pdoJardiPlants.inc.php");
+include("vues/v_entete.php") ;
+include("vues/v_bandeau.php") ;
 
-</body>
-</html>
+if(!isset($_REQUEST['uc']))
+     $uc = 'accueil';
+else
+	$uc = $_REQUEST['uc'];
+
+$pdo = PdoJardiPlants::getPdoJardiPlants();	 
+switch($uc)
+{
+	case 'accueil':
+		{include("vues/v_accueil.php");break;}
+	case 'voirProduits' :
+		{include("controleurs/c_voirProduits.php");break;}
+	case 'gererPanier' :
+		{ include("controleurs/c_gestionPanier.php");break; }
+	case 'supprimerUnProduit' :
+		{ include("controleurs/c_supprimerProduit.php");break; }
+}
+include("vues/v_pied.php") ;
+?>
+
