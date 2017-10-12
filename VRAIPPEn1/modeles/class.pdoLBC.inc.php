bced397633cb722e462d6b43d5f3fecf5c5bb4b0
SIO-SM-49\gourdona <gourdona49@gmail.com>
2017-10-12 11:14:35 +0200

    class pdo

---
+++ VRAIPPEn1/modeles/class.pdoLBC.inc.php
@@ -0,0 +1,37 @@
+<?php
+class pdoLBC	
+{   		
+      	private static $serveur='mysql:host=localhost';
+      	private static $bdd='dbname=salle_communale';   		
+      	private static $user='root' ;    		
+      	private static $mdp='' ;	
+		private static $monPdo;
+		private static $monPdoSalleCommunale = null;
+/**
+ * Constructeur privé, crée l'instance de PDO qui sera sollicitée
+ * pour toutes les méthodes de la classe
+ */				
+	private function __construct()
+	{
+    		pdoLBC::$monPdo = new PDO(PdoSalleCommunale::$serveur.';'.PdoSalleCommunale::$bdd, pdoLBC::$user, pdoLBC::$mdp); 
+			pdoLBC::$monPdo->query("SET CHARACTER SET utf8");
+	}
+	public function _destruct(){
+		pdoLBC::$monPdo = null;
+	}
+/**
+ * Fonction statique qui crée l'unique instance de la classe
+ *
+ * Appel : $instancePdoSalleCommunale = pdoLBC::getpdoLBC();
+ * @return l'unique objet de la classe pdoLBC
+ */
+	public  static function getpdoLBC()
+	{
+		if(pdoLBC::$monpdoLBC == null)
+		{
+			pdoLBC::$monpdoLBC= new pdoLBC();
+		}
+		return pdoLBC::$monpdoLBC;  
+	}
+
+?>
