06a9f21931de28b66577f8abb4652537d13fe23f
SIO-SM-49\nicolass <nicolass@saintemarie-cholet.eu>
2017-10-19 16:09:24 +0200

    index responsables

---
+++ VRAIPPEn1/indexResponsable.php
@@ -0,0 +1,25 @@
+<?php
+require_once("modele/class.pdoLBc.inc.php");
+
+if(!isset($_REQUEST['uc']))
+     $uc = 'accueilResponsable';
+else
+	$uc = $_REQUEST['uc'];
+
+$pdo = PdoLBc::getPdoLBc();	 
+switch($uc)
+{
+	case 'accueilResponsable':
+		{include("controleurs/c_voirResponsable.php");break;}
+	case 'CompteRendu' :
+		{include("controleurs/c_CompteRenduR.php");break;}
+	case 'Somme' :
+		{include("controleurs/c_voirSomme.php");break;}
+	case 'voirActivite' :
+		{include("controleurs/c_ActiviteR.php");break;}
+	case 'inscrireResponsable':
+		{include("controleurs/c_inscrireR.php");break;}
+		{include("controleurs/c_inscrireR.php");break;}
+
+}
+?>
